c15df982497cee06190e73b0c0a3bf1137f88957
inedev905 <122497329+inedev905@users.noreply.github.com>
2023-01-19 23:34:21 +0900

    using

--- Team2_Project/frmNopMiCode.cs
+++ Team2_Project/frmNopMiCode.cs
@@ -5,10 +5,14 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Team2_Project.BaseForms;
+using Team2_Project.Utils;
+using Team2_Project.Services;
+using Team2_Project_DTO;
 
 namespace Team2_Project
 {
-    public partial class frmNopMiCode : Team2_Project.BaseForms.frmCodeControlBase
+    public partial class frmNopMiCode : frmCodeControlBase
     {
         public frmNopMiCode()
         {
