4cc2453366342353421388365ceef4d988e20abc
eunsiri <les5442@gmail.com>
2023-01-19 10:45:02 +0900

    디자인수정

--- Team2_Project/frmUserCode.Designer.cs
+++ Team2_Project/frmUserCode.Designer.cs
@@ -4,14 +4,14 @@ namespace Team2_Project
     partial class frmUserCode
     {
         /// <summary>
-        /// Required designer variable.
+        /// 필수 디자이너 변수입니다.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// 사용 중인 모든 리소스를 정리합니다.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,136 +21,15 @@ namespace Team2_Project
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Windows Form 디자이너에서 생성한 코드
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
         /// </summary>
         private void InitializeComponent()
         {
-            this.pnlSub.SuspendLayout();
-            this.pnlArea.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
-            this.splitContainer2.Panel1.SuspendLayout();
-            this.splitContainer2.Panel2.SuspendLayout();
-            this.splitContainer2.SuspendLayout();
-            this.pnlListTitleL.SuspendLayout();
-            this.pnlListTitleR.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // lblSearchName
-            // 
-            this.lblSearchName.Size = new System.Drawing.Size(138, 17);
-            this.lblSearchName.Text = "사용자정의 대분류명";
-            // 
-            // lblSearchCode
-            // 
-            this.lblSearchCode.Size = new System.Drawing.Size(152, 17);
-            this.lblSearchCode.Text = "사용자정의 대분류코드";
-            // 
-            // lblInfoCodeMa
-            // 
-            this.lblInfoCodeMa.Size = new System.Drawing.Size(152, 17);
-            this.lblInfoCodeMa.Text = "사용자정의 대분류코드";
-            // 
-            // label9
-            // 
-            this.label9.Location = new System.Drawing.Point(539, 199);
-            // 
-            // txtRemark
-            // 
-            this.txtRemark.Location = new System.Drawing.Point(697, 90);
-            // 
-            // label8
-            // 
-            this.label8.Location = new System.Drawing.Point(539, 93);
-            // 
-            // label7
-            // 
-            this.label7.Location = new System.Drawing.Point(539, 41);
-            // 
-            // lblInfoNameDe
-            // 
-            this.lblInfoNameDe.Size = new System.Drawing.Size(152, 17);
-            this.lblInfoNameDe.Text = "사용자정의 상세분류명";
-            // 
-            // lblInfoCodeDe
-            // 
-            this.lblInfoCodeDe.Size = new System.Drawing.Size(166, 17);
-            this.lblInfoCodeDe.Text = "사용자정의 상세분류코드";
-            // 
-            // lblInfoNameMa
-            // 
-            this.lblInfoNameMa.Size = new System.Drawing.Size(138, 17);
-            this.lblInfoNameMa.Text = "사용자정의 대분류명";
-            // 
-            // label14
-            // 
-            this.label14.Location = new System.Drawing.Point(516, 199);
-            // 
-            // label13
-            // 
-            this.label13.Location = new System.Drawing.Point(516, 93);
-            // 
-            // label12
-            // 
-            this.label12.Location = new System.Drawing.Point(516, 41);
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.Size = new System.Drawing.Size(121, 25);
-            // 
-            // pnlListL
-            // 
-            this.pnlListL.Size = new System.Drawing.Size(600, 505);
-            // 
-            // lblListTitleL
-            // 
-            this.lblListTitleL.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            // 
-            // pnlListR
-            // 
-            this.pnlListR.Size = new System.Drawing.Size(1230, 505);
-            // 
-            // lblListTitleR
-            // 
-            this.lblListTitleR.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            // 
-            // splitContainer2
-            // 
-            this.splitContainer2.SplitterDistance = 600;
-            // 
-            // pnlListTitleL
-            // 
-            this.pnlListTitleL.Size = new System.Drawing.Size(600, 43);
-            // 
-            // pnlListTitleR
-            // 
-            this.pnlListTitleR.Size = new System.Drawing.Size(1230, 43);
-            // 
-            // frmUserCode
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1834, 911);
-            this.Name = "frmUserCode";
-            this.Text = "사용자 정의 관리";
-            this.Load += new System.EventHandler(this.frmUserCode_Load);
-            this.pnlSub.ResumeLayout(false);
-            this.pnlSub.PerformLayout();
-            this.pnlArea.ResumeLayout(false);
-            this.pnlArea.PerformLayout();
-            this.splitContainer2.Panel1.ResumeLayout(false);
-            this.splitContainer2.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
-            this.splitContainer2.ResumeLayout(false);
-            this.pnlListTitleL.ResumeLayout(false);
-            this.pnlListTitleL.PerformLayout();
-            this.pnlListTitleR.ResumeLayout(false);
-            this.pnlListTitleR.PerformLayout();
-            this.ResumeLayout(false);
-
+            this.components = new System.ComponentModel.Container();
         }
 
         #endregion
