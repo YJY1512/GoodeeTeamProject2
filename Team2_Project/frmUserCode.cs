64e78defbb9022243cdd3dc7cf729119c6e9b5d0
eunsiri <les5442@gmail.com>
2023-01-20 16:23:24 +0900

    0

--- Team2_Project/frmUserCode.cs
+++ Team2_Project/frmUserCode.cs
@@ -22,7 +22,6 @@ namespace Team2_Project
         private void frmUserCode_Load(object sender, EventArgs e)
         {
             DataGridViewUtil.SetInitDataGridView(dgvMa);
-            DataGridViewUtil.AddGridTextBoxColumn("")
 
             DataGridViewUtil.SetInitDataGridView(dgvMi);
         }
