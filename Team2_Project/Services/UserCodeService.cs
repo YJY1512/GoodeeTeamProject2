433dea41c75fac82b1b7da58c7fc95a1821f5104
eunsiri <les5442@gmail.com>
2023-01-20 17:14:03 +0900

    usercode 코드수정

--- Team2_Project/Services/UserCodeService.cs
+++ Team2_Project/Services/UserCodeService.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Team2_Project_DAO;
+using Team2_Project_DTO;
 
 namespace Team2_Project.Services
 {
-    class UserCodeService
+    public class UserCodeService
     {
+        public List<UserCodeDTO> GetUserCode()
+        {
+            UserCodeDAO db = new UserCodeDAO();
+            List<UserCodeDTO> list = db.GetUserCode();
+
+            return list;
+        }
     }
 }
--- Team2_Project_DAO/UserCodeDAO.cs
+++ Team2_Project_DAO/UserCodeDAO.cs
@@ -22,13 +22,33 @@ namespace Team2_Project_DAO
         }
         public void Dispose()
         {
-           
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
         }
 
-        //public List<UserCodeDTO> GetUserCode()
-        //{
-            
-        //}
-        
+        public List<UserCodeDTO> GetUserCode()
+        {
+            try
+            {
+                string sql = @"select Userdefine_Mi_Code, mi.Userdefine_Ma_Code, Userdefine_Mi_Name, Userdefine_Ma_Name, 
+                                    Sort_Index, mi.Remark, mi.Use_YN
+                            from Userdefine_Mi_Master mi inner join Userdefine_Ma_Master ma 
+                                                        on mi.Userdefine_Ma_Code = ma.Userdefine_Ma_Code and ma.Use_YN = 'Y'";
+
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    conn.Open();
+                    List<UserCodeDTO> list = Helper.DataReaderMapToList<UserCodeDTO>(cmd.ExecuteReader());
+
+                    return list;
+                }
+            }
+            catch (Exception err)
+            {
+                Debug.WriteLine(err.Message);
+                return null;
+            }
+        }
+
     }
 }
