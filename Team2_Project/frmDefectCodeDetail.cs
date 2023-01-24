e9baa8876da8a59dc0ae49311fdc4ae6f40547e8
eunsiri <les5442@gmail.com>
2023-01-24 20:40:17 +0900

    불량코드상세 폼 추가

---
+++ Team2_Project/frmDefectCodeDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Team2_Project
+{
+    public partial class frmDefectCodeDetail : Team2_Project.BaseForms.frmCodeControlBase
+    {
+        public frmDefectCodeDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
