3039a2149519c9cb115be94b8e1930bf92fff942
이승원 <dltmd44@gmail.com>
2023-01-25 20:17:30 +0900

    생산요청

--- Team2_Project_DAO/ItemDAO.cs
+++ Team2_Project_DAO/ItemDAO.cs
@@ -181,7 +181,7 @@ namespace Team2_Project_DAO
         // 완제품 코드랑 이름만 가져오는 코드 - 0124 이승원
         public List<ItemDTO> GetItemCodeName()
         {
-            string sql = @"select SELECT Item_Code , Item_Name FROM Item_Master
+            string sql = @"select Item_Code, Item_Name FROM Item_Master
                             WHERE Use_YN = 'Y'";
 
             using (SqlCommand cmd = new SqlCommand(sql, conn))
