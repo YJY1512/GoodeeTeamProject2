07196aa43d3597bf578be6c9d9f5e87e326b5b92
eunsiri <les5442@gmail.com>
2023-01-26 23:31:07 +0900

    생산계획 화면 디자인 수정

--- Team2_Project/frmPlan.cs
+++ Team2_Project/frmPlan.cs
@@ -12,7 +12,7 @@ using Team2_Project.Utils;
 
 namespace Team2_Project
 {
-    public partial class frmPlan : frmList
+    public partial class frmPlan : Form
     {
         public frmPlan()
         {
@@ -21,16 +21,7 @@ namespace Team2_Project
 
         private void frmOrder_Load(object sender, EventArgs e)
         {
-            DataGridViewUtil.SetInitDataGridView(dataGridView1);
-            DataGridViewUtil.AddGridTextBoxColumn(dataGridView1, "생산계획코드", "", 200);
-            DataGridViewUtil.AddGridTextBoxColumn(dataGridView1, "생산계획일정", "", 200);
-            DataGridViewUtil.AddGridTextBoxColumn(dataGridView1, "상태", "", 200);
 
-            DataGridViewUtil.AddGridTextBoxColumn(dataGridView1, "거래처명", "", 200);
-            DataGridViewUtil.AddGridTextBoxColumn(dataGridView1, "납기일자", "", 200);
-            DataGridViewUtil.AddGridTextBoxColumn(dataGridView1, "품목", "", 200);
-            DataGridViewUtil.AddGridTextBoxColumn(dataGridView1, "품목코드", "", 200);
-            DataGridViewUtil.AddGridTextBoxColumn(dataGridView1, "계획수량", "", 200);
         }
     }
 }
