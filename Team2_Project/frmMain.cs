eecf367e2295293fa40ea4eed63abd8453386838
hjy99977369@gmail.com <hjy99977369@gmail.com>
2023-01-31 00:19:44 +0900

    Main 시간대별실적조회 추가

--- Team2_Project/frmMain.cs
+++ Team2_Project/frmMain.cs
@@ -249,8 +249,7 @@ namespace Team2_Project
             }
             else if (e.Node.Text == "시간대별 실적 조회")
             {
-                //OpenGMMainPage<>();
-                MessageBox.Show("빈 생성 화면입니다.");
+                OpenChildPage<frmTimeProductionHistory>();
             }
             else if (e.Node.Text == "작업지시 현황")
             {
