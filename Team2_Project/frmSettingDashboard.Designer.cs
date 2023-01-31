3f4b5180b7008ffc432c8a7ccea77d5327ace114
inedev905 <122497329+inedev905@users.noreply.github.com>
2023-02-01 01:36:37 +0900

    대시보드 커밋

--- Team2_Project/frmSettingDashboard.Designer.cs
+++ Team2_Project/frmSettingDashboard.Designer.cs
@@ -29,7 +29,82 @@ namespace Team2_Project
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstContent = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTop = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblBottom = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(444, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Content";
+            // 
+            // lstContent
+            // 
+            this.lstContent.FormattingEnabled = true;
+            this.lstContent.ItemHeight = 17;
+            this.lstContent.Location = new System.Drawing.Point(447, 112);
+            this.lstContent.Name = "lstContent";
+            this.lstContent.Size = new System.Drawing.Size(201, 225);
+            this.lstContent.TabIndex = 1;
+            this.lstContent.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lstContent_MouseDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 359);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(489, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "우측 리스트의 항목을 드래그하여 좌측의 짙은 하늘색부분에 올려놓으십시오.";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.lblTop);
+            this.panel1.Location = new System.Drawing.Point(127, 112);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(296, 106);
+            this.panel1.TabIndex = 3;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragOver += new System.Windows.Forms.DragEventHandler(this.panel1_DragOver);
+            // 
+            // lblTop
+            // 
+            this.lblTop.AutoSize = true;
+            this.lblTop.Location = new System.Drawing.Point(145, 47);
+            this.lblTop.Name = "lblTop";
+            this.lblTop.Size = new System.Drawing.Size(19, 17);
+            this.lblTop.TabIndex = 0;
+            this.lblTop.Text = "A";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.lblBottom);
+            this.panel2.Location = new System.Drawing.Point(127, 225);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(296, 106);
+            this.panel2.TabIndex = 4;
+            // 
+            // lblBottom
+            // 
+            this.lblBottom.AutoSize = true;
+            this.lblBottom.Location = new System.Drawing.Point(145, 44);
+            this.lblBottom.Name = "lblBottom";
+            this.lblBottom.Size = new System.Drawing.Size(17, 17);
+            this.lblBottom.TabIndex = 1;
+            this.lblBottom.Text = "B";
             // 
             // frmSettingDashboard
             // 
@@ -37,16 +112,34 @@ namespace Team2_Project
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(828, 470);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstContent);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("나눔고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmSettingDashboard";
-            this.Text = "frmSettingDashboard";
+            this.Text = "SettingDashboard";
             this.Load += new System.EventHandler(this.frmSettingDashboard_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstContent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblTop;
+        private System.Windows.Forms.Label lblBottom;
     }
 }
