c4d73f814935fe5ae6096ef53be6bd2de38d76f4
hjy99977369@gmail.com <hjy99977369@gmail.com>
2023-02-03 10:29:15 +0900

    daswqef

---
+++ Team2_Project_DTO/MenuDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team2_Project_DTO
+{
+    public class MenuDTO
+    {
+        public string Screen_Code        { get; set; }
+        public string Parent_Screen_Code { get; set; }
+        public int    Sort_Index         { get; set; }
+        public string Type               { get; set; }
+        public string Form_Name          { get; set; }
+        public string Menu_Name          { get; set; }
+        public string Menu_Image         { get; set; }
+        public int    Menu_Level         { get; set; }
+        public string Use_YN             { get; set; }
+    }
+}
