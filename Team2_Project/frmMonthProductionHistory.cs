c59c516ad9efbf222a97974c31b40dd618a689da
inedev905 <122497329+inedev905@users.noreply.github.com>
2023-02-14 13:54:17 +0900

    0

--- Team2_Project/frmMonthProductionHistory.cs
+++ Team2_Project/frmMonthProductionHistory.cs
@@ -52,28 +52,22 @@ namespace Team2_Project
             DataGridViewUtil.AddGridTextBoxColumn(dgvData, "일평균생산수량", "AverageDailyProduction", 120, DataGridViewContentAlignment.MiddleRight);
             DataGridViewUtil.AddGridTextBoxColumn(dgvData, "월평균생산수량", "AverageMonthProduction", 150, DataGridViewContentAlignment.MiddleRight); //일일생산량?
             DataGridViewUtil.AddGridTextBoxColumn(dgvData, "생산일수", "TotalProductionDays", 120, DataGridViewContentAlignment.MiddleRight);
-
             string[] dotCell = new string[] { "TotPlanQty", "TotInQty", "TotOutQty", "TotPrdQty", "AverageMonthProduction", "TotalProductionDays", "TotDefectQty" };
             foreach (string item in dotCell) dgvData.Columns[item].DefaultCellStyle.Format = "N0";
 
-            string[] dotPersentCell = new string[] { "AttainmentRate", "QualityRate", "OperatingRate", "DefectRate" };
+            //string[] dotPersentCell = new string[] { "AttainmentRate", "QualityRate", "OperatingRate", "DefectRate" };
             //foreach (string item in dotPersentCell)
             //{
             //    dgvData.Columns[item].DefaultCellStyle.Format = "N0";
             //    //dgvData.Columns[item].DefaultCellStyle.FormatProvider = CultureInfo.InvariantCulture;
             //}
 
-                dgvData.ColumnHeadersDefaultCellStyle.Font = dgvData.DefaultCellStyle.Font = new Font("나눔고딕", 11);
+            dgvData.ColumnHeadersDefaultCellStyle.Font = dgvData.DefaultCellStyle.Font = new Font("나눔고딕", 11);
             this.dgvData.Columns["Item_Name"].Frozen = true;
             dgvData.MultiSelect = false;
             dgvData.ClearSelection();
-
             OnSearch();
-
             rdoPrdQty.Checked = rdoChartTwo.Checked = true;
-            //---- test ----
-            //ChartData();
-            //--------------
         }
 
         private void AdvancedListBind(List<MonthProductionHistoryDTO> datasource, DataGridView dgv)
@@ -103,9 +97,7 @@ namespace Team2_Project
                 }
 
                 if (dgvData.Rows.Count > 0) //데이터가 있을 때
-                {
                     ChartData();
-                }
             }
             else
             {
@@ -113,7 +105,6 @@ namespace Team2_Project
                 chtDataPie.Series.Clear();
                 chtDataLine.Series.Clear();
             }
-
         }
 
         public void OnReLoad()  //새로고침
@@ -156,25 +147,20 @@ namespace Team2_Project
                     chtDataPie.Series.Clear();
                     chtDataLine.Series.Clear();
 
-
                     chtDataPie.Series.Add(chartTitle);
                     chtDataPie.Series[chartTitle].Points.Clear();
                     chtDataPie.Series[chartTitle].ChartType = SeriesChartType.Doughnut;
                     chtDataPie.Series[chartTitle].IsValueShownAsLabel = true;
 
-                    if (rdoChartTwo.Checked)
-                    {
-                        chtDataLine.Series.Add(chartTitle);
-                        chtDataLine.Series[chartTitle].Points.Clear();
-                        chtDataLine.Series[chartTitle].ChartType = SeriesChartType.StackedColumn;
-                        chtDataLine.Series[chartTitle].IsValueShownAsLabel = true;
-                    }
+                    chtDataLine.Series.Add(chartTitle);
+                    chtDataLine.Series[chartTitle].Points.Clear();
+                    chtDataLine.Series[chartTitle].ChartType = SeriesChartType.StackedColumn;
+                    chtDataLine.Series[chartTitle].IsValueShownAsLabel = true;
 
                     foreach (var item in MTHistoryList)
                     {
-                        string itemName = item.Item_Name;
                         int TotQty = 0;
-
+                        string itemName = item.Item_Name;
                         if (chartTitle.Equals("월별 제품 생산비율")) TotQty = item.TotPrdQty;
                         else if (chartTitle.Equals("월별 제품 목표량")) TotQty = item.TotPlanQty;
                         else if (chartTitle.Equals("월별 제품 투입량")) TotQty = item.TotInQty;
@@ -187,19 +173,16 @@ namespace Team2_Project
                         //chtDataPie.Series[chartTitle].Points[num].Color = Color.FromArgb(211 + colors, 226, 223);
                         //chtDataPie.Series[chartTitle].Points[num].LabelFormat = "N2";
 
-                        if (rdoChartTwo.Checked)
-                        {
-                            chtDataLine.Series[chartTitle].Points.AddXY(itemName, TotQty);
-                            chtDataLine.Series[chartTitle].BorderColor = Color.Gray;
-                            chtDataLine.Series[chartTitle].Points[num].LegendText = itemName;
-                            //.Series[chartTitle].Points[num].Color = Color.FromArgb(211 + colors, 226, 223);
-                            //chtDataLine.Series[chartTitle].Points[num].LabelFormat = "#,###";
-                        }
+                        chtDataLine.Series[chartTitle].Points.AddXY(itemName, TotQty);
+                        chtDataLine.Series[chartTitle].BorderColor = Color.Gray;
+                        chtDataLine.Series[chartTitle].Points[num].LegendText = itemName;
+                        //.Series[chartTitle].Points[num].Color = Color.FromArgb(211 + colors, 226, 223);
+                        //chtDataLine.Series[chartTitle].Points[num].LabelFormat = "#,###";
                         num++;
                         colors += 7;
                     }
                 }
-            }            
+            }
         }
 
         private CommonPop<ItemDTO> GetItemPopInfo()
@@ -209,17 +192,13 @@ namespace Team2_Project
                 ItemService srv = new ItemService();
                 itemList = srv.GetItemCodeName();
             }
-
             CommonPop<ItemDTO> itemPopInfo = new CommonPop<ItemDTO>();
             itemPopInfo.DgvDatasource = itemList;
             itemPopInfo.PopName = "품목 검색";
-
             List<DataGridViewTextBoxColumn> colList = new List<DataGridViewTextBoxColumn>();
             colList.Add(DataGridViewUtil.ReturnNewDgvColumn("품목코드", "Item_Code", 200));
             colList.Add(DataGridViewUtil.ReturnNewDgvColumn("품목명", "Item_Name", 200));
-
             itemPopInfo.DgvCols = colList;
-
             return itemPopInfo;
         }
 
@@ -230,7 +209,7 @@ namespace Team2_Project
 
         private void rdoChecked_CheckedChanged(object sender, EventArgs e)
         {
-            if(dgvData.Rows.Count > 0 && dgvData.Columns.Count > 0)
+            if (dgvData.Rows.Count > 0 && dgvData.Columns.Count > 0)
             {
                 foreach (DataGridViewColumn col in dgvData.Columns)
                 {
@@ -253,23 +232,21 @@ namespace Team2_Project
                 }
                 else if (rdoOutQty.Checked) //산출량
                 {
-                    backColorCell = new string[] { "TotPlanQty", "TotOutQty", "AttainmentRate" , "QualityRate" }; //목표량, 산출수량, 달성율, 양품률
+                    backColorCell = new string[] { "TotPlanQty", "TotOutQty", "AttainmentRate", "QualityRate" }; //목표량, 산출수량, 달성율, 양품률
                 }
                 else if (rdoLossQty.Checked) //Loss수량
                 {
                     backColorCell = new string[] { "TotInQty", "TotOutQty", "TotDefectQty", "DefectRate" }; //투입수량, 산출수량, 불량수량, 불량비율
                 }
-
                 foreach (string item in backColorCell)
                     dgvData.Columns[item].DefaultCellStyle.BackColor = Color.FromArgb(236, 236, 236);
 
-
-                if (dgvData.Rows.Count > 0) 
+                if (dgvData.Rows.Count > 0)
                     ChartData();/* dgvData_CellClick(dgvData.CurrentRow.Index, new DataGridViewCellEventArgs(0, 0));*/
                 else chtDataPie.Series.Clear();
 
                 dgvData.ClearSelection();
-            }                
+            }
         }
 
         private void rdoChartTwo_CheckedChanged(object sender, EventArgs e)
@@ -289,12 +266,10 @@ namespace Team2_Project
         private void dgvData_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
         {
             DataGridViewColumn column = dgvData.Columns[e.ColumnIndex];
-
             if (column.HeaderText == "달성률") column.HeaderCell.ToolTipText = "(생산수량 / 목표수량) *100";
             else if (column.HeaderText == "양품률") column.HeaderCell.ToolTipText = "(산출수량 / 투입수량) *100";
             else if (column.HeaderText == "가동률") column.HeaderCell.ToolTipText = "(투입수량 / 목표수량) *100";
             else if (column.HeaderText == "불량률") column.HeaderCell.ToolTipText = "(불량수량 / 생산수량 ) *100";
-
         }
     }
 }
